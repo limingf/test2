dcc57e886c170b31c87178a2c066df60cbf28d98
lmning <258139710@qq.com>
2014-10-11 12:30:49 +0800

    add by limingf 20141011

---
+++ 1.sql
@@ -0,0 +1,3 @@
+insert into t3 (id, code, name) values ('100', '100', 't1name100');
+insert into t3 (id, code, name) values ('101', '101', 't1name101');
+insert into t3 (id, code, name) values ('102', '102', 't1name102');
