a828b6b0f144ebe3e84829d7c0f468a6b98ef922
limingf <limingf@yonyou.com>
2014-11-19 14:47:08 +0800

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -1,3 +1,4 @@
 insert into t3 (id, code, name) values ('100', '100', 't1name100');
 insert into t3 (id, code, name) values ('101', '101', 't1name101');
 insert into t3 (id, code, name) values ('102', '102', 't1name102');
+insert into t3 (id, code, name) values ('104', '104', 't1name104');
