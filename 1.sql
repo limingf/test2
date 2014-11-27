694adb904759228a089fe232289f8f6704fc0a32
limingf <limingf@yonyou.com>
2014-11-27 10:03:47 +0800

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -4,3 +4,5 @@ insert into t3 (id, code, name) values ('102', '102', 't1name102');
 insert into t3 (id, code, name) values ('103', '103', 't1name103');
 insert into t3 (id, code, name) values ('104', '104', 't1name104');
 insert into t3 (id, code, name) values ('105', '105', 't1name105');
+//add by limingf
+insert into t3 (id, code, name) values ('106', '106', 't1name106');
