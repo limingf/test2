bead96262692107ecaeb122509421b6742b17317
limingf <limingf@yonyou.com>
2015-01-05 09:33:37 +0800

    rm more files

--- 1.sql
+++
@@ -1,8 +0,0 @@
-insert into t3 (id, code, name) values ('100', '100', 't1name100');
-insert into t3 (id, code, name) values ('101', '101', 't1name101');
-insert into t3 (id, code, name) values ('102', '102', 't1name102');
-insert into t3 (id, code, name) values ('103', '103', 't1name103');
-insert into t3 (id, code, name) values ('104', '104', 't1name104');
-insert into t3 (id, code, name) values ('105', '105', 't1name105');
-//add by limingf
-insert into t3 (id, code, name) values ('106', '106', 't1name106');
